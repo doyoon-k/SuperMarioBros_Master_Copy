5194fc88fe717542e166aa3c00f5551dbe43adda
SeungGeon Kim <keithrek@hanmail.net>
2019-09-26 03:16:46 +0900

    Development Envionment Setup Done

---
+++ main.js
@@ -0,0 +1,24 @@
+/*
+  main.js
+  Super Mario Bros.
+
+  GAM100 
+  Fall 2019
+
+  JunHo Hwang did ---
+  DoYun Kim did ---
+  SeungGeon Kim did ---
+  
+  All content © 2019 DigiPen (USA) Corporation, all rights reserved.
+*/
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(55);
+
+  //Does ES6 work?
+  let newMario = new mario();
+}
---
+++ mario.js
@@ -0,0 +1,17 @@
+/*
+  main.js
+  Super Mario Bros.
+
+  GAM100 
+  Fall 2019
+
+  JunHo Hwang did ---
+  DoYun Kim did ---
+  SeungGeon Kim did ---
+  
+  All content © 2019 DigiPen (USA) Corporation, all rights reserved.
+*/
+
+class mario {
+  
+}
