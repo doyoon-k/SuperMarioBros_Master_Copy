19305e4206dd7bcbf7f9414647c43d67221d837c
dpkoreastub <dpkoreastub@hotmail.com>
2019-09-30 18:42:23 +0900

    hitbox class

---
+++ hitbox.js
@@ -0,0 +1,24 @@
+/*
+  helper_functions.js
+  Super Mario Bros.
+
+  GAM100
+  Fall 2019
+
+  JoonHo Hwang did ---
+  DoYoon Kim did ---
+  SeungGeon Kim did ---
+
+  All content © 2019 DigiPen (USA) Corporation, all rights reserved.
+*/
+
+class Hitbox
+{
+    constructor(x,y,width,height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
